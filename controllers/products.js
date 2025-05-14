c524145b191d0dba9745ec45ab7fdbcd0c1f4ec2
Maldoseri23 <maldoseria@gmail.com>
2025-05-14 18:41:05 +0300

    created index.ejs page for products

--- controllers/products.js
+++ controllers/products.js
@@ -4,4 +4,12 @@ const router = express.Router();
 
 const Product = require('../models/product');
 
+// Routes/ API's/ Functionality
+
+router.get('/', (req,res) => {
+  const products = products.find()
+  res.render('products/index.ejs', {products})
+})
+
+
 module.exports = router;
