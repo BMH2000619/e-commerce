d81e016b7054e7cee9b3a885b5c2f32854bd593c
BMH2000619 <baselmohammedb.m@gmail.com>
2025-05-14 20:30:07 +0300

    Added RESTful Routes for orders

--- models/order.js
+++ models/order.js
@@ -1,20 +1,24 @@
 const mongoose = require('mongoose')
 
+const orderItemSchema = new mongoose.Schema({
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product',
+    required: true
+  },
+  quantity: {
+    type: Number,
+    min: 1,
+    required: true
+  }
+})
+
 const orderSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User'
   },
-  item: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      product: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Product'
-      },
-      quantity: { type: Number, min: 1 }
-    }
-  ],
+  item: [orderItemSchema],
   total: {
     type: Number,
     required: true
