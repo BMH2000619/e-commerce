cdcce87b437ba7a22be035a5f1a379b6e6b88c9f
Maldoseri23 <maldoseria@gmail.com>
2025-05-14 15:18:43 +0300

    built product model

---
+++ models/product.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category'
+  },
+  description: {
+    type: String
+  } ,
+  price: {
+    type: Number,
+    required: true,
+    min: 0
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 1
+  },
+  img: {
+    type: String,
+    required: true
+  }
+})
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product
