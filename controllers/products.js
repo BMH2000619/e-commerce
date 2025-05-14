936a2bdfcc3f4c8d180249ec1fc7120f290362d1
Maldoseri23 <maldoseria@gmail.com>
2025-05-14 20:35:36 +0300

    created form to add new products and added functionality

--- controllers/products.js
+++ controllers/products.js
@@ -3,12 +3,19 @@ const express = require('express');
 const router = express.Router();
 
 const Product = require('../models/product');
+// There is no mo
+const Category = require('./models/Category');
 
 // Routes/ API's/ Functionality
 
-router.get('/', (req,res) => {
-  const products = products.find()
+router.get('/', async (req,res) => {
+  const products = await Product.find()
   res.render('products/index.ejs', {products})
+})
+
+router.get('/new', async (req,res) => {
+  const categories = await Category.find();
+  res.render('products/new.ejs', {categories})
 })
 
 
