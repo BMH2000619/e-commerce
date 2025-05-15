83cac13eedcbcd74c219331d3bfccaf3e0979de1
Jaffar43 <mr.jaffarali2001@gmail.com>
2025-05-15 16:05:00 +0300

    Creating a session for a signed-in user

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,26 +8,59 @@ router.get("/sign-up", (req, res) => {
 });
 
 router.post("/sign-up", async (req, res) => {
-  res.send("Form submission accepted!");
+  const userInDatabase = await User.findOne({ username: req.body.username });
+if (userInDatabase) {
+  return res.send("Username already taken.");
+}
+
+const validPassword = bcrypt.compareSync(
+  req.body.password,
+  userInDatabase.password
+);
+if (!validPassword) {
+  return res.send("Login failed. Please try again.");
+}
+
+const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+req.body.password = hashedPassword;
+
+
+const user = await User.create(req.body);
+
+req.session.user = {
+  username: userInDatabase.username,
+};
+
+req.session.save(() => {
+  res.redirect("/");
+})
+
 });
 
 router.get("/sign-in", (req, res) => {
   res.render("auth/sign-in.ejs");
 });
 
-const userInDatabase = await User.findOne({ username: req.body.username });
-if (userInDatabase) {
-  return res.send("Username already taken.");
+router.post("/sign-in", async (req, res) => {
+  const userInDatabase = await User.findOne({ username: req.body.username });
+if (!userInDatabase) {
+  return res.send("Login failed. Please try again.");
 }
 
-const hashedPassword = bcrypt.hashSync(req.body.password, 10);
-req.body.password = hashedPassword;
-
-if (req.body.password !== req.body.confirmPassword) {
-  return res.send("Password and Confirm Password must match");
+const validPassword = bcrypt.compareSync(
+  req.body.password,
+  userInDatabase.password
+);
+if (!validPassword) {
+  return res.send("Login failed. Please try again.");
 }
 
-const user = await User.create(req.body);
-res.send(`Thanks for signing up ${user.username}`);
+req.session.user = {
+  username: userInDatabase.username,
+    _id: userInDatabase._id
+};
+
+res.redirect("/");
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -38,6 +38,7 @@ app.use(
   })
 );
 
+
 app.use(passUserToView);
 
 // Require controller
