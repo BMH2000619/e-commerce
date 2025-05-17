f45f71df0983dc1c90bc7dd4aac8e291f5475000
BMH2000619 <baselmohammedb.m@gmail.com>
2025-05-17 16:39:29 +0300

    Added 2nd order route

--- controllers/orders.js
+++ controllers/orders.js
@@ -13,3 +13,27 @@ router.get('/', async (req, res) => {
 
   res.render('orders/index.ejs', { orders })
 })
+
+
+// POST /orders - create new order from user's active cart
+router.post('/', async (req, res) => {
+  try {
+    const userId = req.session.user._id;
+
+    const activeCart = await Cart.findOne({ user: userId, status: 'active' });
+    if (!activeCart) {
+      return res.send('No active cart found.');
+    }
+
+    const order = await Order.create({ cartId: activeCart._id });
+
+    // Mark the cart as inactive since it's been ordered
+    activeCart.status = 'inactive';
+    await activeCart.save();
+
+    res.redirect(`/orders/${order._id}`);
+  } catch (err) {
+    console.error(err);
+    res.redirect('/');
+  }
+});
