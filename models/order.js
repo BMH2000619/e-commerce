9c75b152139a616a5f2b25bc8cd1d1e9c34bcc80
BMH2000619 <baselmohammedb.m@gmail.com>
2025-05-17 16:31:50 +0300

    Removed orderItemSchema

--- models/order.js
+++ models/order.js
@@ -1,18 +1,5 @@
 const mongoose = require('mongoose')
 
-const orderItemSchema = new mongoose.Schema({
-  product: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Product',
-    required: true
-  },
-  quantity: {
-    type: Number,
-    min: 1,
-    required: true
-  }
-})
-
 const orderSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
